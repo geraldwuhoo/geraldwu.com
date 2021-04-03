25b251dc4fffce99e84369d24ad634da9243e35d
98WuG <gerald@geraldwu.com>
2021-04-03 14:40:51 -0400

    embed repo information into the page with gitlab CI/CD variables

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import jinja2
 import yaml
 import markdown
+import os
 
 def main():
     config = 'config.yaml'
     template = 'index.jinja2'
     outputfile = 'index.html'
 
+    # Load in config file
     with open(config) as f:
         data = yaml.safe_load(f)
 
-    for job in data["experience"]:
-        job["description"] = markdown.markdown(job["description"])[3:-4]
+    # Convert descriptions from markdown to html
+    def convert(to_convert):
+        for item in to_convert:
+            item['description'] = markdown.markdown(item['description'])[3:-4]
+    convert(data['experience'])
+    convert(data['projects'])
+    
+    # Fill in predefined variables from GitLab CI/CD
+    data['commit'] = os.getenv('CI_COMMIT_SHORT_SHA', 'N/A')
+    data['repo'] = os.getenv('CI_REPOSITORY_URL', '#')
+    data['pipeline'] = os.getenv('CI_PIPELINE_URL', '#')
+    data['pipeline_id'] = os.getenv('CI_PIPELINE_ID', 'N/A')
 
-    for project in data["projects"]:
-        project["description"] = markdown.markdown(project["description"])[3:-4]
-
+    # Execute the jinja2 template
     subs = jinja2.Environment(
         loader=jinja2.FileSystemLoader('./')
     ).get_template(template).render(data=data)
 
+    # Save jinja2 templating result
     with open(outputfile, 'w') as f:
         f.write(subs)
 
